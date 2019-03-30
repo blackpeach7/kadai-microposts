61e42068006cc671248a78acb15040c3b15d5926
blackpeach <cococyi@yahoo.co.jp>
2019-03-30 08:34:58 +0000

    follow, unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,21 @@ class User < ApplicationRecord
     has_secure_password
     
     has_many :microposts
+    has_many :relationships
+    has_many :followings, through: :relationships, source: :follow
+    has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+    has_many :followers, through: :reverses_of_relationship, source: :user
+
+  def follow(other_user)
+      self.relationships.find_or_created_by(follow_id: other_user.id)
+  end
+  
+  def unfollow(other_user)
+      follow = self.relationships.find_by(follow_id: other_user.id)
+      follow.destroy if (follow != nil)
+  end
+  
+  def following?(other_user)
+      self.followings.include?(other_user)
+  end      
 end
